0220f23135fd8673e05637c8d96e8627cda450e4
Cristian Chitiva <cristianchitiva7@gmail.com>
2026-03-03 13:39:51 +0100

    Improve comments in kill.sh for clarity and maintainability

--- tmux/kill.sh
+++ tmux/kill.sh
@@ -9,6 +9,6 @@ cd "$SCRIPT_PATH"
 export TMUX_SESSION_NAME=ros2_pkg_template
 export TMUX_SOCKET_NAME=f4f
 
-# just attach to the session
-tmux -L $TMUX_SOCKET_NAME split-window -t $TMUX_SESSION_NAME
-tmux -L $TMUX_SOCKET_NAME send-keys -t $TMUX_SESSION_NAME "sleep 1; tmux list-panes -s -F \"#{pane_pid} #{pane_current_command}\" | grep -v tmux | cut -d\" \" -f1 | while read in; do killp \$in; done; exit" ENTER
+# Create a new pane and run the command to kill all non-tmux processes in the session
+tmux -L "$TMUX_SOCKET_NAME" split-window -t "$TMUX_SESSION_NAME"
+tmux -L "$TMUX_SOCKET_NAME" send-keys -t "$TMUX_SESSION_NAME" "sleep 1; tmux list-panes -s -F \"#{pane_pid} #{pane_current_command}\" | grep -v tmux | cut -d\" \" -f1 | while read in; do kill \$in; done; exit" ENTER
